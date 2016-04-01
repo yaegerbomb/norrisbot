25a70392b44bb848d81f14e90fc04c3a1beb7e00
David Yaeger <gvyaeger@gmail.com>
2016-04-01 10:25:03 -0400

    first commit

---
+++ bot.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var NorrisBot = require('../lib/norrisbot');
+
+var token = process.env.BOT_API_KEY;
+var dbPath = process.env.BOT_DB_PATH;
+var name = process.env.BOT_NAME;
+
+var norrisbot = new NorrisBot({
+    token: 'xoxb-31293820775-PAXjjhoBIcjxjcQnmZ1KubMy',
+    dbPath: dbPath,
+    name: 'norrisbot'
+});
+
+norrisbot.run();
